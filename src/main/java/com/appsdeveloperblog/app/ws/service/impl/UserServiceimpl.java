22e65523f610a802d2bd9bf53b08806324e4a5cf
Ben Meyer <bmeyer@Bens-Mac-mini.local>
2019-02-24 00:01:19 -0500

    Added delete action

--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceimpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceimpl.java
@@ -73,7 +73,7 @@ public class UserServiceImpl implements UserService {
 		UserDto returnValue = new UserDto();
 		UserEntity userEntity = userRepository.findByUserId(userId);
 
-		if(userEntity == null) throw new UsernameNotFoundException(userId);
+		if(userEntity == null) throw new UsernameNotFoundException("User with ID: " + userId + " not found");
 
 		BeanUtils.copyProperties(userEntity, returnValue);
 		return returnValue;
@@ -94,6 +94,13 @@ public class UserServiceImpl implements UserService {
 		BeanUtils.copyProperties(updatedUserDetails, returnValue);
 
 		return returnValue;
+	}
+
+	@Override
+	public void deleteUser(String userId) {
+		UserEntity userEntity = userRepository.findByUserId(userId);
+		if(userEntity == null) throw new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
+		userRepository.delete(userEntity);
 	}
 
 }
