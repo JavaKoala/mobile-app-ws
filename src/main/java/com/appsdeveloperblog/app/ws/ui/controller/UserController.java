88eda8bbc70306216b94bfb092adaf10487a8f68
Ben Meyer <bmeyer@Bens-Mac-mini.local>
2019-03-02 22:49:49 -0500

    Added verifyEmailToken method to UserController

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -168,6 +168,21 @@ public class UserController {
 		addressesRestModel.add(userLink);
 		addressesRestModel.add(addressesLink);
 		return new Resource<>(addressesRestModel);
-
 	}
+
+	// http://localhost:8080/mobile-app-ws/users/email-verification?token=abc123
+	@GetMapping(path = "/email-verification", produces = { MediaType.APPLICATION_XML_VALUE,
+					     MediaType.APPLICATION_JSON_VALUE })
+	public OperationStatusModel verifyEmailToken(@RequestParam(value = "token") String token) {
+		OperationStatusModel returnValue = new OperationStatusModel();
+		returnValue.setOperationName(RequestOperationName.VERIFY_EMAIL.name());
+		boolean isVerified = userService.verifyEmailToken(token);
+		if(isVerified) {
+			returnValue.setOperationResult(RequestOperationStatus.SUCCESS.name());
+		} else {
+			returnValue.setOperationResult(RequestOperationStatus.ERROR.name());
+		}
+		return returnValue;
+	}
+
 }
--- src/main/java/com/appsdeveloperblog/app/ws/ui/model/response/RequestOperationName.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/model/response/RequestOperationName.java
@@ -2,5 +2,5 @@ package com.appsdeveloperblog.app.ws.ui.model.response;
 
 public enum RequestOperationName {
 
-	DELETE
+	DELETE, VERIFY_EMAIL
 }
