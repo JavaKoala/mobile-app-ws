3ec50cc563bbea6366945b6c00b1a40404906bf6
Ben Meyer <bmeyer@Bens-Mac-mini.local>
2019-03-27 22:43:22 -0400

    Updated API information in swagger

--- src/main/java/com/appsdeveloperblog/app/ws/SwaggerConfig.java
+++ src/main/java/com/appsdeveloperblog/app/ws/SwaggerConfig.java
@@ -1,10 +1,18 @@
 package com.appsdeveloperblog.app.ws;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,9 +21,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	Contact contact = new Contact(
+			"JavaKoala",
+			"https://github.com/JavaKoala/mobile-app-ws",
+			"test@test.com");
+
+	List<VendorExtension> vendorExtensions = new ArrayList<>();
+
+	ApiInfo apiInfo = new ApiInfo("Photo app RESTful Web Service Doucmentation",
+			"These pages documents Photo app RESTful Web Service enpoints",
+			"1.0",
+			"https://github.com/JavaKoala/mobile-app-ws",
+			contact,
+			"Apache 2.0",
+			"https://www.apache.org/licenses/LICENSE-2.0",
+			vendorExtensions);
 	@Bean
 	public Docket apiDocket() {
 		Docket docket = new Docket(DocumentationType.SWAGGER_2)
+				.protocols(new HashSet<>(Arrays.asList("HTTP", "HTTPs")))
+				.apiInfo(apiInfo)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.appsdeveloperblog.app.ws"))
 				.paths(PathSelectors.any())
